0c45d588657ccb3ccafc31b3fce1460f10c67516
tempusername5 <buttercorn285@gmail.com>
2024-01-11 22:23:06 -0700

    Add CSV file writing

--- lincolnwiscraper/lincolnwiscraper/spiders/lincolnwispider.py
+++ lincolnwiscraper/lincolnwiscraper/spiders/lincolnwispider.py
@@ -1,5 +1,6 @@
 import scrapy
-
+import datetime
+import csv
 
 class LincolnwispiderSpider(scrapy.Spider):
     name = "lincolnwispider"
@@ -7,4 +8,17 @@ class LincolnwispiderSpider(scrapy.Spider):
     start_urls = ["https://co.lincoln.wi.us/meetings"]
 
     def parse(self, response):
-        pass
+        date = response.css("td.views-field-field-calendar-date span.date-display-single::text").get()
+        title = response.css("td.views-field-title::text").get()
+        self.save_to_csv([["date", "meeting_title", "category", "URL"], [date, title, "", ""]])
+
+    def generate_timestamp(self) -> str:
+        return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    def save_to_csv(self, contents):
+        file_name = "CO_LINCOLN_WI_US_MEETINGS-metadata-" + self.generate_timestamp() + ".csv"
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for i in range(len(contents)):
+                writer.writerow(contents[i])
+
